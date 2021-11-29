76a56cfadd44eb9029efba23fde2c410c598bdb9
Reima Kuomanen <reimkuos@gmail.com>
2021-11-29 22:47:36 +0200

    Ostos olioiden palautetun maaran tarkistus

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -62,3 +62,14 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
         self.assertEqual(ostos.lukumaara(), 1)
+
+
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio(self):
+        maito = Tuote("Maito", 3)
+        piima = Tuote("Piima", 4)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(piima)
+ 
+        ostokset = self.kori.ostokset()
+ 
+        self.assertEqual(len(ostokset), 1)
