af9400967109690934aed3f3d7caf4d949976a35
Reima Kuomanen <reimkuos@gmail.com>
2021-11-29 22:30:08 +0200

    Lisätty monen tuotteen hinnan tarkastus

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -26,3 +26,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(piima)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_hinta_on_oikea(self):
+        maito = Tuote("Maito", 3)
+        piima = Tuote("Piima", 4)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(piima)
+        self.assertEqual(self.kori.hinta(), 7)
